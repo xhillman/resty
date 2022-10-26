da5ebcce51a684ca22cbb067f26e2d85304a1a62
Xavier Hillman <xhillman13@gmail.com>
2022-10-26 17:45:51 -0400

    update to unit tests

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -3,7 +3,7 @@ import './results.scss'
 const Results = (props) => {
   return (
     <section data-testid='output-area'>
-      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      <pre data-testid='results-pre'>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
     </section>
     // <section>
     //   <pre>{props.data ? JSON.stringify(props.data.count, props.data.headers, props.data.body) : null}</pre>
